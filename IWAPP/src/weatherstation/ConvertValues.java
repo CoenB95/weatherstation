e17383770ea085ec96e588880e07d154246962aa
dokugan <stijn-28@hotmail.com>
2016-09-13 14:40:56 +0200

    heatindexwindchill

--- IWAPP/src/weatherstation/ConvertValues.java
+++ IWAPP/src/weatherstation/ConvertValues.java
@@ -30,7 +30,7 @@ public class ConvertValues
 	* @param mval the value from the database.
 	* @return the temperature in degrees Celsius.
 	*/
-	public static double temperature(double mval)
+	public static double temperature(short mval)
 	{
 		double celsius;
 		celsius = (mval/10 - 32)/ 1.8;
@@ -43,7 +43,7 @@ public class ConvertValues
 	 * @param mval the value from the database.
 	 * @return the wind speed in km/h. 
 	*/
-	public static double windSpeed(double mval)
+	public static double windSpeed(short mval)
 	{
 		double kph;
 		kph = mval * 1.609344;
@@ -56,7 +56,7 @@ public class ConvertValues
 	* @param mval the value from the database.
 	* @return the amount of rain in mm.
 	*/	
-	public static double rainAmount(double mval)
+	public static double rainAmount(short mval)
 	{
 		double rain;
 		rain = mval *0.2;
@@ -70,7 +70,7 @@ public class ConvertValues
 	* @param mval the value from the database.
 	* @return the uv index.
 	*/
-	public static double uvIndex(double mval)
+	public static double uvIndex(short mval)
 	{
 		double uv;
 		uv = mval/10;
@@ -83,7 +83,7 @@ public class ConvertValues
 	* @param mval the value from the database.
 	* @return the battery voltage.
 	*/
-	public static double batteryVoltage(double mval)
+	public static double batteryVoltage(short mval)
 	{
 		double batt = mval * 0.005859375;
 		return batt;
@@ -101,6 +101,39 @@ public class ConvertValues
 		int minutes = mval%100;
 		int hours = (mval-minutes)/100;
 		return hours + ":" + minutes;
+	}
+	
+	/**
+	 * humidity has to be 50%
+	 * 
+	 * @param mval1
+	 * @param mval2
+	 * @return
+	 */
+	public static double dewpoint(short mval1, short mval2)
+	{
+		double temp = temperature(mval1);
+		short hum = mval2;
+		double dewp = temp - ((100-hum)/5);
+		return dewp;
+	}
+	
+	public static double windchill(short mval1, short mval2)
+	{
+		double windspd = windSpeed(mval2);
+		double temp = temperature(mval1);
+		
+		if (temp < 10 && temp > -47)
+		{
+			double windChill = 13.12 + (.6215 * temp)+(0.3965 *(temp-28.676) * (Math.pow(windspd, 0.16)));
+			return windChill;
+		}
+		return 0;
+	}
+	
+	public static double heatindex(short mval1, short mval2)
+	{
+		return 1;
 	}
 	
 	public static void main(String[] args)
