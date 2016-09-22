3a3c54585b78d5bb33948876d84eed3fc6bd03ea
Coen <boelhouwers.coen@gmail.com>
2016-09-22 11:23:09 +0200

    Added Measurement class.

---
+++ IWAPP/src/weatherstation/Measurement.java
@@ -0,0 +1,110 @@
+package weatherstation;
+
+import java.sql.Timestamp;
+
+import weatherstation.sql.RawMeasurement;
+
+public class Measurement {
+
+	private RawMeasurement rawMeasurement;
+	
+	public Measurement(RawMeasurement rawMeasurement) {
+		this.rawMeasurement = rawMeasurement;
+	}
+	
+	public double getAverageWindSpeed() {
+		return ValueConverter.windSpeed(rawMeasurement.getAvgWindSpeed());
+	}
+	
+	public double getBarometer() {
+		return ValueConverter.convertBarometer(rawMeasurement.getBarometer());
+	}
+	
+	public double getBatteryVoltage() {
+		return ValueConverter.batteryVoltage(rawMeasurement.getBattLevel());
+	}
+	
+	public Timestamp getDateStamp() {
+		return rawMeasurement.getDateStamp();
+	}
+	
+	public double getDewPoint() {
+		return ValueConverter.dewpoint(rawMeasurement.getOutsideTemp(), 
+				rawMeasurement.getOutsideHum());
+	}
+	
+	public double getHeatIndex() {
+		return ValueConverter.heatindex(rawMeasurement.getOutsideTemp(), 
+				rawMeasurement.getOutsideHum());
+	}
+	
+	public double getInsideHumidity() {
+		return rawMeasurement.getInsideHum();
+	}
+	
+	public double getInsideTemperature() {
+		return ValueConverter.temperature(rawMeasurement.getInsideTemp());
+	}
+	
+	public double getOutsideHumidity() {
+		return rawMeasurement.getOutsideHum();
+	}
+	
+	public double getOutsideTemperature() {
+		return ValueConverter.temperature(rawMeasurement.getOutsideTemp());
+	}
+	
+	public double getRainRate() {
+		return ValueConverter.rainAmount(rawMeasurement.getRainRate());
+	}
+	
+	public double getSolarRadiation() {
+		return rawMeasurement.getSolarRad();
+	}
+	
+	public String getSunrise() {
+		return ValueConverter.sunrise(rawMeasurement.getSunrise());
+	}
+	
+	public String getSunset() {
+		return ValueConverter.sunset(rawMeasurement.getSunset());
+	}
+	
+	public double getUVLevel() {
+		return ValueConverter.uvIndex(rawMeasurement.getUVLevel());
+	}
+	
+	public double getWindChill() {
+		return ValueConverter.windchill(rawMeasurement.getOutsideTemp(),
+				rawMeasurement.getWindSpeed());
+	}
+	
+	public double getWindDirection() {
+		return rawMeasurement.getWindDir();
+	}
+	
+	public double getWindSpeed() {
+		return ValueConverter.windSpeed(rawMeasurement.getWindSpeed());
+	}
+	
+	@Override
+	public String toString() {
+        String s = "Measurement:"
+            + "\ndateStamp = \t" + getDateStamp()
+            + "\nbarometer = \t" + getBarometer()
+            + "\ninsideTemp = \t" + getInsideTemperature()
+            + "\ninsideHum = \t" + getInsideHumidity()
+            + "\noutsideTemp = \t" + getOutsideTemperature()
+            + "\nwindSpeed = \t" + getWindSpeed()
+            + "\navgWindSpeed = \t" + getAverageWindSpeed()
+            + "\nwindDir = \t" + getWindDirection()
+            + "\noutsideHum = \t" + getOutsideHumidity()
+            + "\nrainRate = \t" + getRainRate()
+            + "\nUVLevel = \t" + getUVLevel()
+            + "\nsolarRad = \t" + getSolarRadiation()
+            + "\nbattLevel = \t" + getBatteryVoltage()
+            + "\nsunrise = \t" + getSunrise()
+            + "\nsunset = \t" + getSunset();
+        return s; 
+    } 
+}
--- IWAPP/src/weatherstation/test/Tester.java
+++ IWAPP/src/weatherstation/test/Tester.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import weatherstation.ConvertValues;
+import weatherstation.ValueConverter;
 
 /**
  * The JUnit test-class for this application.
@@ -19,24 +19,24 @@ public class Tester {
 	@Test
 	public void test() {
 		//TODO: define a test for air pressure
-		//assertEquals(?, ConvertValues.airPressure(?), ?);
-		assertEquals(26.39, ConvertValues.temperature((short) 795), 0.01);
-		assertEquals(51.5, ConvertValues.windSpeed((short) 32), 0.01);
-		assertEquals(51.2, ConvertValues.rainAmount((short) 256), 0.01);
-		assertEquals(6.6, ConvertValues.uvIndex((short) 66), 0.01);
+		//assertEquals(?, ValueConverter.airPressure(?), ?);
+		assertEquals(26.39, ValueConverter.temperature((short) 795), 0.01);
+		assertEquals(51.5, ValueConverter.windSpeed((short) 32), 0.01);
+		assertEquals(51.2, ValueConverter.rainAmount((short) 256), 0.01);
+		assertEquals(6.6, ValueConverter.uvIndex((short) 66), 0.01);
 		//Should this expected value be more hardcoded?
-		assertEquals(100*300.0/512.0/100.0, ConvertValues.batteryVoltage((short) 100), 0.01);
-		assertEquals("06:50", ConvertValues.sunrise((short) 650));
-		assertEquals("06:50", ConvertValues.sunset((short) 650));
+		assertEquals(100*300.0/512.0/100.0, ValueConverter.batteryVoltage((short) 100), 0.01);
+		assertEquals("06:50", ValueConverter.sunrise((short) 650));
+		assertEquals("06:50", ValueConverter.sunset((short) 650));
 		
 		//TODO: define a test for dew point
-		//assertEquals(0, ConvertValues.dewpoint((short) 0, (short) 0), 0.01);
+		//assertEquals(0, ValueConverter.dewpoint((short) 0, (short) 0), 0.01);
 		
 		//TODO: define a test for wind chill
-		//assertEquals(0, ConvertValues.windchill((short) 0, (short) 0), 0.01);
+		//assertEquals(0, ValueConverter.windchill((short) 0, (short) 0), 0.01);
 		
 		//TODO: define a test for heat index
-		//assertEquals(0, ConvertValues.heatindex((short) 0, (short) 0), 0.01);
+		//assertEquals(0, ValueConverter.heatindex((short) 0, (short) 0), 0.01);
 	}
 
 }
