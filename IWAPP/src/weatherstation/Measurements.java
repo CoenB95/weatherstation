498b3479b668236e7e8ddec3e2e5282ca9c25301
arjangosens <arjangosens@hotmail.com>
2016-10-06 15:54:02 +0200

    Added getAverage method to Measurements

--- IWAPP/src/weatherstation/Measurements.java
+++ IWAPP/src/weatherstation/Measurements.java
@@ -12,7 +12,7 @@ public class Measurements {
 
 	private WeatherStation station;
 	private List<Measurement> measurements;
-	
+
 	/**
 	 * @deprecated A period should be set from the beginning.
 	 */
@@ -20,17 +20,17 @@ public class Measurements {
 		station = new WeatherStation();
 		measurements = new ArrayList<>();
 	}
-	
+
 	public Measurements(LocalDate d) {
 		this(d, d);
 	}
-	
+
 	public Measurements(LocalDate d1, LocalDate d2) {
 		station = new WeatherStation();
 		measurements = new ArrayList<>();
 		setPeriod(d1, d2);
 	}
-	
+
 	public void setPeriod(LocalDate d1, LocalDate d2) {
 		station.getAllMeasurementsBetween(d1, d2);
 		for (RawMeasurement rm : station.getAllMeasurementsBetween(d1, d2)) {
@@ -38,7 +38,7 @@ public class Measurements {
 		}
 	}
 
-	
+
 	/**
 	 * Returns the max bv temperature of each day in the period.
 	 * @param field
@@ -62,7 +62,7 @@ public class Measurements {
 		result.add(max);
 		return result;
 	}
-	
+
 	public List<Double> getLowest(int field) {
 		List<Double> result = new ArrayList<>();
 		if (measurements.isEmpty()) return result;
@@ -79,6 +79,37 @@ public class Measurements {
 				min = m.getDouble(field);
 		}
 		result.add(min);
+		return result;
+	}
+	
+	/**
+	 *  This method calculates the average of the values specified in the MainApp.
+	 *  It requests a List from the MainApp with values depending on the field variable.
+	 *  It then reads out the current date and walks through the specified dates, where it calculates the average
+	 *  for each day. After that it calculates the total average using the averages given per day.
+	 * @param field
+	 * @return
+	 */
+	public List<Double> getAverage(int field) {
+		double total = 0;
+		List<Double> result = new ArrayList<>();
+		if (measurements.isEmpty()) return result;
+		LocalDateTime date = measurements.get(0).getDateStamp();
+		double avg = 0;
+		for (Measurement m:measurements) {
+			if (m.getDateStamp().getDayOfYear() > date.getDayOfYear() ||
+					m.getDateStamp().getDayOfYear() == 0) {
+				date = m.getDateStamp();
+				avg = avg / total;
+				result.add(avg);
+				avg = 0;
+				total = 0;
+			}
+			avg += m.getDouble(field);
+			total++;
+		}
+		avg = avg / total;
+		result.add(avg);
 		return result;
 	}
 }
