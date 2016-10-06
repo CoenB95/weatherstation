1c252545d8b1c3c288cd0e1c5f02a790f394a586
nardswolfs <nard.swolfs@hotmail.com>
2016-10-06 12:07:29 +0200

    fuck dis

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/4/301ead61ac8b00161894dfdc50a2516c
@@ -0,0 +1,5 @@
+package weatherstation;
+
+public class Menu {
+
+}
--- IWAPP/src/weatherstation/Menu.java
+++ IWAPP/src/weatherstation/Menu.java
@@ -1,5 +1,24 @@
 package weatherstation;
 
-public class Menu {
+import java.util.ArrayList;
 
+public class Menu {
+	private ArrayList<String> menu;
+	
+	public Menu(String[] values)
+	{
+		menu = new ArrayList<>();
+		for(String value: values){
+			menu.add(value);
+		}
+	}
+	
+	/**
+	 * adds an option to the menu
+	 * @param option
+	 */
+	public void addOption(String option)
+	{
+		menu.add(option);
+	}
 }
