b4163bb0b11e6b187a3ec89947687bd18252af0d
Coen <boelhouwers.coen@gmail.com>
2016-10-06 09:06:44 +0200

    Some boilerplate.

--- IWAPP/src/weatherstation/Measurements.java
+++ IWAPP/src/weatherstation/Measurements.java
@@ -1,12 +1,15 @@
 package weatherstation;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import weatherstation.sql.RawMeasurement;
 import weatherstation.sql.WeatherStation;
 
 public class Measurements {
 
 	private WeatherStation station;
+	private List<Measurement> measurements;
 	
 	public Measurements() {
 		station = new WeatherStation();
@@ -23,5 +26,8 @@ public class Measurements {
 	
 	public void setPeriod(LocalDate d1, LocalDate d2) {
 		station.getAllMeasurementsBetween(d1, d2);
+		for (RawMeasurement rm : station.getAllMeasurementsBetween(d1, d2)) {
+			measurements.add(new Measurement(rm));
+		}
 	}
 }
