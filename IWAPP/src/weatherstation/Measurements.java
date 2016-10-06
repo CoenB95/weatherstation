471e5b4c7bfafbe9fef3d56b98fe7fd0d9400bac
Coen <boelhouwers.coen@gmail.com>
2016-10-06 10:12:16 +0200

    getHighest idea. Implement it!

--- IWAPP/src/weatherstation/Measurements.java
+++ IWAPP/src/weatherstation/Measurements.java
@@ -1,6 +1,9 @@
 package weatherstation;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import weatherstation.sql.RawMeasurement;
@@ -29,5 +32,26 @@ public class Measurements {
 		for (RawMeasurement rm : station.getAllMeasurementsBetween(d1, d2)) {
 			measurements.add(new Measurement(rm));
 		}
+	}
+	
+	/**
+	 * Returns the max bv temperature of each day in the period.
+	 * @param field
+	 * @return
+	 */
+	public List<Double> getHighest(int field) {
+		List<Double> result = new ArrayList<>();
+		if (measurements.isEmpty()) return result;
+		LocalDateTime date = measurements.get(0).getDateStamp();
+		double max = 0;//measurements.get(0).get(field);
+		for (Measurement m:measurements) {
+			if (m.getDateStamp().getDayOfYear() > date.getDayOfYear() ||
+					m.getDateStamp().getDayOfYear() == 0) {
+				result.add(max);
+			}
+			//if (m.get(field) > max) 
+			//	max = m.get(field);
+		}
+		return result;
 	}
 }
